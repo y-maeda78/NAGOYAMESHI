3917fa250703ba26b63279f7c6f657baa805fcab
y-maeda78 <prknowledge.my78@gmail.com>
2025-12-11 12:06:31 +0900

    1211 commit edit:adminsummary

--- base/views/adminsummary_views.py
+++ base/views/adminsummary_views.py
@@ -19,9 +19,9 @@ class SalesSummaryView(StaffRequiredMixin, TemplateView):
         MONTHLY_FEE = 300   # サービス設定金額
 
         # 総売上と総会員数の集計
-        total_users = User.objects.filter(is_active=True).count() # 総会員数
+        total_users = User.objects.filter(is_active=True,is_admin=False).count() # 総会員数
         total_subscriptionusers = User.objects.filter(is_paymentstatus=True,is_admin=False).count() # 有料会員数
-        total_revenue = Order.objects.filter(is_confirmed=True).aggregate(Sum('amount'))['amount__sum'] or 0
+        total_revenue = Order.objects.filter(is_confirmed=True,is_admin=False).aggregate(Sum('amount'))['amount__sum'] or 0
 
         context['total_users'] = total_users
         context['total_subscriptionusers'] = total_subscriptionusers
